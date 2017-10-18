22cb68c9bcff373d9a8a5785301255cc26a5a23f
Lauri Rooden <lauri@rooden.ee>
2017-10-18 12:46:24 +0300

    model: Add attrs to Item#toJSON()

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ require("./format-spec.js")
 require("./model/index.js")
 require("./model/filter.js")
 require("./model/acl.js")
+require("./model/tojson.js")
 require("./getSchema-spec.js")
 require("./schema-apply.js")
 
