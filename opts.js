cfd1c7070db2ad037e6a6bc9febcea4c8e5af31d
Lauri Rooden <lauri@rooden.ee>
2024-02-17 16:36:29 +0200

    opts: Handle short options

--- opts.js
+++ opts.js
@@ -1,62 +1,73 @@
 
+/* litejs.com/MIT-LICENSE.txt */
 
 exports.opts = opts
 
 function opts(defaults, argv) {
-	var UNDEF, key, val, expect
-	, i = 0
-	, commands = {}
-	, out = {_: [], _used: [], _unknown: []}
-	, hasOwn = out.hasOwnProperty
+	var expect, key, val
+	, i = -1
+	, short = {}
+	, hasOwn = short.hasOwnProperty
+	, out = {_: [], _valid: [], _unknown: []}
 	, isElectronBinary = process.versions.electron && !process.defaultApp
 
 	if (!argv) argv = process.argv.slice(isElectronBinary ? 1 : 2)
 
-	for (key in defaults) if (isObj(defaults[key])) {
-		val = key.split(/[,_]/)
-		commands[val[0]] = Object.assign({_cmd: val[0]}, defaults[key])
-		for (i = val.length; --i; ) commands[val[i]] = commands[val[0]]
-	} else {
-		out[key] = defaults[key]
+	for (; (val = argv[++i]); ) if (val[0] !== "-") {
+		for (key in defaults) if (isObj(defaults[key])) {
+			expect = key.split("_")
+			if (expect[0] === val || expect[1] === val) {
+				out._cmd = expect[0]
+				argv.splice(i, 1)
+				defaults = Object.assign({}, defaults, defaults[key])
+				break
+			}
+		}
+		break
 	}
 
-	for (i = 0; i < argv.length; i++) {
-		val = argv[i]
-		if (val.charAt(0) !== "-") {
-			if (isObj(commands[val])) {
-				argv.splice(i, 1)
-				defaults = Object.assign({}, Object.assign(out, commands[val]))
-			}
-			break
+	for (key in defaults) if (!isObj(defaults[key])) {
+		val = key.split("_")
+		if (val[0]) {
+			out[val[0]] = defaults[key]
+			if (val[1]) short[val[1]] = val[0]
 		}
 	}
 
 	for (i = 0; i < argv.length; i++) {
-		val = argv[i].split(/=|--(no-)?/)
-		if (val[0] === "") {
-			key = val[2].replace(/\-([a-z])/g, camelFn)
+		val = argv[i].split(/=|--(no-)?|(^-)/)
+		if (val[0] === "" && argv[i][0] !== "=") {
+			key = val[2] && short[val[3][0]]
+			if (key) {
+				expect = val[3].slice(1)
+				if (typeof out[key] === "boolean" && expect) {
+					argv.splice(i--, 1, "--" + key, "-" + expect)
+					continue
+				}
+				val[3] = val[6] = key
+				argv[i] = "--" + key + "=" + (expect || argv.splice(i + 1, 1))
+			}
+			key = val[3].replace(/\-([a-z])/g, camelFn)
 			if (key === "") {
-				argv.push.apply(out._, argv.splice(i).slice(1))
+				argv.push.apply(out._, argv.slice(i + 1))
 				break
 			}
 			if (hasOwn.call(out, key)) {
 				expect = Array.isArray(out[key]) ? "array" : typeof out[key]
-				val = val[1] ? 0 : val[4]
+				val = val[1] ? 0 : typeof val[6] === "string" && argv[i].slice(val[3].length + 3)
 				out[key] = (
-					expect === "boolean" ? val !== 0 && (val === UNDEF || val === "true" || val !== "false" && castError()) :
+					expect === "boolean" ? val !== 0 && (val === false || val === "true" || val !== "false" && castError()) :
 					expect === "array" ? (out[key] === defaults[key] ? [] : out[key]).concat(val ? val.split(",") : []) :
 					expect === "string" ? val || "" :
-					expect === "number" && val !== UNDEF ? +val :
+					expect === "number" && val !== false ? +val :
 					castError()
 				)
-				out._used.push(argv[i])
-				argv.splice(i--, 1)
+				out._valid.push(argv[i])
 			} else {
-				out._unknown.push(key)
+				out._unknown.push(argv[i])
 			}
 		} else {
 			out._.push(argv[i])
-			argv.splice(i--, 1)
 		}
 	}
 	return out
--- test/opts.js
+++ test/opts.js
@@ -4,45 +4,53 @@ describe("opts", function() {
 
 	it("should parse options: {0}", [
 		[
-			["--b1", "--no-b2", "--b3=true", "--b4=false"],
+			["--b1", "--no-b2", "--b3=true", "--b4=false", "r"],
 			{ b1: false, b2: false, b3: false, b4: false },
-			{ b1: true,  b2: false, b3: true, b4: false, _used: ["--b1", "--no-b2", "--b3=true", "--b4=false"], _unknown: [], _: [] }, []
+			{ b1: true,  b2: false, b3: true, b4: false, _valid: ["--b1", "--no-b2", "--b3=true", "--b4=false"], _unknown: [], _: ["r"] }
 		],
 		[
-			["--b5", "--no-b6", "--b7=true", "--b8=false"],
+			["--b5", "--no-b6", "--b7=true", "r", "--b8=false"],
 			{ b5: true, b6: true,  b7: true, b8: true },
-			{ b5: true, b6: false, b7: true, b8: false, _used: ["--b5", "--no-b6", "--b7=true", "--b8=false"], _unknown: [], _: [] }, []
+			{ b5: true, b6: false, b7: true, b8: false, _valid: ["--b5", "--no-b6", "--b7=true", "--b8=false"], _unknown: [], _: ["r"] }
 		],
 		[
-			["--no-a1", "--a2=", "--a3=foo,bar" ],
+			["r", "--no-a1", "--a2=", "--a3=foo,bar", "R"],
 			{ a1: [ 1 ], a2: [ 2 ], a3: [ 3 ] },
-			{ a1: [], a2: [], a3: ["foo", "bar"], _used: ["--no-a1", "--a2=", "--a3=foo,bar"], _unknown: [], _: [] }, []
+			{ a1: [], a2: [], a3: ["foo", "bar"], _valid: ["--no-a1", "--a2=", "--a3=foo,bar"], _unknown: [], _: ["r", "R"] }
 		],
 		[
-			["--ab=foo", "--ab=bar,qux" ],
+			["--ab=foo", "--ab=bar,qux", "=2"],
 			{ ab: [ 1 ] },
-			{ ab: [ "foo", "bar", "qux" ], _used: ["--ab=foo", "--ab=bar,qux"], _unknown: [], _: [] }, []
+			{ ab: [ "foo", "bar", "qux" ], _valid: ["--ab=foo", "--ab=bar,qux"], _unknown: [], _: ["=2"] }
 		],
 		[
 			["--n1=12", "--n2=0", "--no-n3"],
 			{ n1: 0, n2: 1, n3: 2 },
-			{ n1: 12, n2: 0, n3: 0, _used: ["--n1=12", "--n2=0", "--no-n3"], _unknown: [], _: [] }, []
+			{ n1: 12, n2: 0, n3: 0, _valid: ["--n1=12", "--n2=0", "--no-n3"], _unknown: [], _: [] }
 		],
 		[
 			["--s1=", "--s2=1", "--s3=a", "--no-s4"],
 			{s1:"a",s2:"",s3:"1",s4:"2"},
-			{s1:"",s2:"1",s3:"a",s4:"", _used: ["--s1=", "--s2=1", "--s3=a", "--no-s4"],_unknown:[],_:[]}, []
+			{s1:"",s2:"1",s3:"a",s4:"", _valid: ["--s1=", "--s2=1", "--s3=a", "--no-s4"],_unknown:[],_:[]}
+		],
+		[
+			["--s1=--a", "--s2=1--", "--s3=a--b"],
+			{s1:"",s2:"",s3:""},
+			{s1:"--a",s2:"1--",s3:"a--b", _valid: ["--s1=--a", "--s2=1--", "--s3=a--b"],_unknown:[],_:[]}
 		],
 		[
 			["--s1=a b", "--http-port=81"],
-			{s1:"", httpPort: 80}, {s1:"a b", httpPort: 81, _used: ["--s1=a b", "--http-port=81"], _unknown:[],_:[]}, []
+			{s1:"", httpPort_h: 80}, {s1:"a b", httpPort: 81, _valid: ["--s1=a b", "--http-port=81"], _unknown:[],_:[]}
 		],
-		[ [], {print:false, to:"a", x:true}, {print:false, to:"a", x:true, _used: [], _unknown:[],_:[]}, [] ]
-	], function(argv, defaults, expected, remain, assert) {
+		[
+			["-abcd","-e", "f", "-g"],
+			{a_a:false, b_b:false, c_c:"",e_e:"",g_g:"h"}, {a:true, b:true, c:"d", e:"f",g:"", _valid: ["--a", "--b","--c=d", "--e=f","--g="], _unknown:[],_:[]}
+		],
+		[ [], {print:false, to:"a", x:true}, {print:false, to:"a", x:true, _valid: [], _unknown:[],_:[]} ]
+	], function(argv, defaults, expected, assert) {
 		var result = opts(defaults, argv)
 		assert
 		.equal(result, expected)
-		.equal(argv, remain)
 		.end()
 	})
 
@@ -50,14 +58,13 @@ describe("opts", function() {
 		[
 			["--b1"],
 			{ b2: false },
-			{ b2: false, _used: [],  _unknown: [ "b1" ], _: [] }, ["--b1"]
+			{ b2: false, _valid: [],  _unknown: [ "--b1" ], _: [] }
 		],
-		[ ["--invalid", "--no-opt"], {}, {_used: [], _unknown:["invalid", "opt"],_:[]}, ["--invalid", "--no-opt"] ],
-	], function(argv, defaults, expected, remain, assert) {
+		[ ["--invalid", "--no-opt"], {}, {_valid: [], _unknown:["--invalid", "--no-opt"],_:[]} ],
+	], function(argv, defaults, expected, assert) {
 		var result = opts(defaults, argv)
 		assert
 		.equal(result, expected)
-		.equal(argv, remain)
 		.end()
 	})
 
@@ -65,23 +72,22 @@ describe("opts", function() {
 		[
 			["--", "--bee", "a"],
 			{ a: false },
-			{ a: false, _used: [],  _unknown: [], _: ["--bee", "a"] }, []
+			{ a: false, _valid: [],  _unknown: [], _: ["--bee", "a"] }
 		],
 		[
 			["--a", "--"],
 			{ a: false },
-			{ a: true, _used: ["--a"],  _unknown: [], _: [] }, []
+			{ a: true, _valid: ["--a"],  _unknown: [], _: [] }
 		],
 		[
 			["--a", "--", "--no-a"],
 			{ a: false },
-			{ a: true, _used: ["--a"],  _unknown: [], _: ["--no-a"] }, []
+			{ a: true, _valid: ["--a"],  _unknown: [], _: ["--no-a"] }
 		],
-	], function(argv, defaults, expected, remain, assert) {
+	], function(argv, defaults, expected, assert) {
 		var result = opts(defaults, argv)
 		assert
 		.equal(result, expected)
-		.equal(argv, remain)
 		.end()
 	})
 
@@ -89,18 +95,17 @@ describe("opts", function() {
 		[
 			["--b1", "a.txt", "b.txt"],
 			{ b1: false },
-			{ b1: true, _used: ["--b1"],  _unknown: [], _: [ "a.txt", "b.txt" ] }, []
+			{ b1: true, _valid: ["--b1"],  _unknown: [], _: [ "a.txt", "b.txt" ] }
 		],
 		[
 			["a.txt", "--b1", "b.txt"],
 			{ b1: false },
-			{ b1: true, _used: ["--b1"],  _unknown: [], _: [ "a.txt", "b.txt" ] }, []
+			{ b1: true, _valid: ["--b1"],  _unknown: [], _: [ "a.txt", "b.txt" ] }
 		],
-	], function(argv, defaults, expected, remain, assert) {
+	], function(argv, defaults, expected, assert) {
 		var result = opts(defaults, argv)
 		assert
 		.equal(result, expected)
-		.equal(argv, remain)
 		.end()
 	})
 
@@ -108,28 +113,27 @@ describe("opts", function() {
 		[
 			["--no-a", "comm", "--b", "--c"],
 			{ comm: { a: true }, other: { c: 1 }, b: false },
-			{ a: false, b: true, _: [], _cmd: "comm", _used: ["--no-a", "--b"],  _unknown: ["c"] }, ["--c"]
+			{ a: false, b: true, _: [], _cmd: "comm", _valid: ["--no-a", "--b"],  _unknown: ["--c"] }
 		],
 		[
 			["--no-a", "--b", "--c"],
 			{ comm: { a: true }, other: { c: 1 }, b: false },
-			{ b: true, _: [], _used: ["--b"], _unknown: ["a", "c"] }, ["--no-a", "--c"]
+			{ b: true, _: [], _valid: ["--b"], _unknown: ["--no-a", "--c"] }
 		],
 		[
 			["comm", "--comm", "--other"],
 			{ comm: { comm: false, other: false }, other: { c: 1 }, b: false },
-			{ b: false, comm: true, other: true, _: [], _cmd: "comm", _used: ["--comm", "--other"],  _unknown: [] }, []
+			{ b: false, comm: true, other: true, _: [], _cmd: "comm", _valid: ["--comm", "--other"],  _unknown: [] }
 		],
 		[
 			["b", "--build"],
 			{ build_b: { build: false }, bench: {} },
-			{ build: true, _: [], _cmd: "build", _used: ["--build"],  _unknown: [] }, []
+			{ build: true, _: [], _cmd: "build", _valid: ["--build"],  _unknown: [] }
 		],
-	], function(argv, defaults, expected, remain, assert) {
+	], function(argv, defaults, expected, assert) {
 		var result = opts(defaults, argv)
 		assert
 		.equal(result, expected)
-		.equal(argv, remain)
 		.end()
 	})
 	it("should throw on invalid options: {0}", [
