4d926eee0ccd92d91b08664014f3da0b5433751e
Lauri Rooden <lauri@rooden.ee>
2024-06-17 03:05:27 +0300

    test: Compare all built files

--- test/build.js
+++ test/build.js
@@ -7,9 +7,9 @@ describe("build", function() {
 	require("../snapshot.js")
 
 	describe("file {0}", [
-		["build-cp",     [], [/*"build/a.svg", "build/b.svg",*/ "build/c.svg"]],
-		["build-inline", ["lib1.css", "lib2.css"], ["lib1.css", "lib2.css"]],
-		["build-simple", [], []],
+		["build-cp"],
+		["build-inline"],
+		["build-simple"],
 		//["test/ui/dev.html", "test/ui/index.html", [
 		//	"test/ui/lib1.css", "test/ui/lib2.css"
 		//]],
@@ -18,33 +18,40 @@ describe("build", function() {
 		//	"test/ui/min/manifest.json",
 		//	"test/ui/min/image.svg"
 		//]]
-	], function(name, sameFolderFiles, buildFolderFiles) {
+	], function(name) {
 		test("to {0}", [
-			[ "same folder", "", sameFolderFiles ],
-			[ "build folder", "build/", buildFolderFiles ],
-		], function(x, outDir, files, assert, mock) {
-			var temp = "test/build-temp-" + assert.i + "/"
-			, prefix = "test/data/snap/" + assert.i + "-"
+			[ "same folder", "" ],
+			[ "build folder", "build/" ],
+		], function(x, outDir, assert, mock) {
+			var temp = "test/data/temp/" + assert.i + "/"
+			, prefix = "test/data/snap/" + assert.i + "/"
 
 			mock.swap(console, "error", mock.fn())
-			cli.mkdirp(temp + outDir)
 			cli.cp("test/data/" + name, temp)
+			var initialFiles = cli.ls(temp + "**/*", { dir: false })
 
-			assert.planned = files.length + 2
+			cli.mkdirp(temp + outDir)
 
 			build({ _: [temp + "dev.html"], out: temp + outDir + "index.html" })
+			var createdFiles = cli.ls(temp + "**/*", { dir: false }).filter(function(file) {
+				return initialFiles.indexOf(file) < 0
+			})
+			assert.planned = initialFiles.length + createdFiles.length + 2
 			assert.cmdSnapshot("cd " + temp + ";find .|sort", prefix + "ls")
 			assert.matchSnapshot(temp + outDir + "index.html", 0, prefix + "index.html")
-			files.forEach(function(name) {
+			initialFiles.forEach(function(file) {
+				file = file.replace("index.html", "dev.html")
+				assert.matchSnapshot(file.replace("/temp/" + assert.i, "/" + name), 0, file)
+			})
+			createdFiles.forEach(function(file) {
 				try {
-					assert.matchSnapshot(temp + name, 0, prefix + name)
+					assert.matchSnapshot(file, 0, file.replace("/temp/", "/snap/"))
 				} catch(e) {
 					assert(false, e)
 				}
 			})
 			cli.rmrf(temp)
 		})
-
 	})
 })
 
---
+++ test/data/snap/4/lib1.js
@@ -0,0 +1,7 @@
+
+var load = [/*!{loadFiles}*/]
+
+
+var a = 1;
+
+var b = 2
